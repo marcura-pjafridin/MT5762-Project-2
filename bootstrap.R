d52ac5aa904ebacd2aa716c252d1b56c6d6b3f86
Roca65 <55445329+Roca65@users.noreply.github.com>
2019-10-29 20:37:12 +0800

    Update bootstrap.R

--- bootstrap.R
+++ bootstrap.R
@@ -5,18 +5,18 @@ bootstrapping <- function(formula, data, indices) {
   fit <- lm(formula, data = d)
   return(coef(fit))
 }
-#Then use the function to resample 1000 replications
+#Then using the function to resample 1000 replications
 library(boot)
 set.seed(2345)
 results <- boot(data = ModelData, statistic = bootstrapping,
                 R = 1000, 
-                formula = wt...7 ~ id + gestation + age + ht + drace + dwt + time)
+                formula = wt...7 ~   id + date + gestation + ht + drace + dage + dwt + number)
 print(results)
 
 #Add an index parameter to the plot() and boot.ci() function
 #To indicate which column of object to analyze
-#index 2 is id, index 3 is gestation, index 4 is age, index 5 is ht, 
-#index 6 is drace, index 7 = dwt, index 8 is time
+#index 2 is id, index 3 is date, index 4 is gestation, index 5 is ht, 
+#index 6 is drace, index 7 = dage, index 8 is dwt, index 9 is number
 #To polt the results for each index
 plot(results, index = 1)
 plot(results, index = 2)
@@ -26,8 +26,10 @@ plot(results, index = 5)
 plot(results, index = 6)
 plot(results, index = 7)
 plot(results, index = 8)
+plot(results, index = 9)
 
-#To generate the 95% confidence interval for “id + gestation + age + ht + drace + dwt + time”
+
+#To generate the 95% confidence interval for “id + date + gestation + ht + drace + dage + dwt + number”
 boot.ci(results, type = "bca", index = 1)
 boot.ci(results, type = "bca", index = 2)
 boot.ci(results, type = "bca", index = 3)
@@ -36,9 +38,4 @@ boot.ci(results, type = "bca", index = 5)
 boot.ci(results, type = "bca", index = 6)
 boot.ci(results, type = "bca", index = 7)
 boot.ci(results, type = "bca", index = 8)
-
-
-
-
-
-
+boot.ci(results, type = "bca", index = 9)
