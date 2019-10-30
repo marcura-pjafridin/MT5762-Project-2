b663d7890759608deae61c63c874dbefe42175e8
Roca65 <55445329+Roca65@users.noreply.github.com>
2019-10-31 07:38:59 +0800

    Update bootstrap.R

--- bootstrap.R
+++ bootstrap.R
@@ -10,14 +10,20 @@ library(boot)
 set.seed(2345)
 results <- boot(data = ModelData, statistic = bootstrapping,
                 R = 1000, 
-                formula = wt...7 ~ gestation + smoke + ht + drace + parity + dht + id)
+                formula = wt...7 ~  id + gestation + parity + ht + drace + dht + time)
 print(results)
+
 
 #Add an index parameter to the plot() and boot.ci() function
 #To indicate which column of object to analyze
-#index 2 is id, index 3 is date, index 4 is gestation, index 5 is ht, 
-#index 6 is drace, index 7 = dage, index 8 is dwt, index 9 is number
+#index 2 is id, index 3 is gestation, index 4 is parity, index 5 is ht, 
+#index 6 is drace, index 7 = dht, index 8 is time
 #To polt the results for each index
+#8 plots in total
+for (i in 1:8) {
+  plot(results, index = i)
+}
+
 plot(results, index = 1)
 plot(results, index = 2)
 plot(results, index = 3)
@@ -26,10 +32,10 @@ plot(results, index = 5)
 plot(results, index = 6)
 plot(results, index = 7)
 plot(results, index = 8)
-plot(results, index = 9)
 
 
-#To generate the 95% confidence interval for “gestation + smoke + ht + drace + parity + dht + id”
+#To generate the 95% confidence interval for “id + gestation + parity + ht + drace + dht + time”
+
 boot.ci(results, type = "bca", index = 1)
 boot.ci(results, type = "bca", index = 2)
 boot.ci(results, type = "bca", index = 3)
@@ -38,4 +44,3 @@ boot.ci(results, type = "bca", index = 5)
 boot.ci(results, type = "bca", index = 6)
 boot.ci(results, type = "bca", index = 7)
 boot.ci(results, type = "bca", index = 8)
-boot.ci(results, type = "bca", index = 9)
