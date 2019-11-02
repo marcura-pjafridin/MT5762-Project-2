d5af0733a7b1f8064be2a40531a0d73a9ff79b20
Roca65 <55445329+Roca65@users.noreply.github.com>
2019-11-03 02:26:05 +0800

    Update Project 2.R

--- Project 2.R
+++ Project 2.R
@@ -319,39 +319,4 @@ y_stepmod <- as.matrix(y_stepmod)
 MSE[1, 2] <- k_fold(StepMod, k=5, x = x_stepmod, y = y_stepmod)  
 #The result show that the formod is better because the value of 5-fold is much smaller
 
-# ----------------------------------------- BOOTSTRAPPING -------------------------------------------- ##
-#Establish a function that returns the vector of regression coefficient 
-bootstrapping <- function(formula, data, indices) {
-  d <- data[indices,]
-  fit <- lm(formula, data = d)
-  return(coef(fit))
-}
-#Then using the function to resample 1000 replications
-library(boot)
-set.seed(2345)
-results <- boot(data = ModelData, statistic = bootstrapping,
-                R = 1000, 
-                formula = wt...7 ~ gestation + smoke + ht + drace + parity + dht)
-print(results)
 
-
-#Add an index parameter to the plot() and boot.ci() function
-#To indicate which column of object to analyze
-#index 2 is gestation, index 3 is smoke, index 4 is ht, index 5 is drace, 
-#index 6 is parity, index 7 = dht
-#To polt the results for each index
-#8 plots in total
-for (i in 1:7) {
-  plot(results, index = i)
-}
-
-
-#To generate the 95% confidence interval for “gestation + smoke + ht + drace + parity + dht + id”
-
-boot.ci(results, type = "bca", index = 1)
-boot.ci(results, type = "bca", index = 2)
-boot.ci(results, type = "bca", index = 3)
-boot.ci(results, type = "bca", index = 4)
-boot.ci(results, type = "bca", index = 5)
-boot.ci(results, type = "bca", index = 6)
-boot.ci(results, type = "bca", index = 7)
