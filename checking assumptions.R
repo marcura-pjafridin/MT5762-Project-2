45077d744639d0553c7388034839c8a0e9b06bc0
RobinWHM <53634096+RobinWHM@users.noreply.github.com>
2019-11-02 12:55:17 +0000

    UPDATED

--- checking assumptions.R
+++ checking assumptions.R
@@ -1,24 +1,24 @@
-############### check assumptions #################
-
+# ------------------------------------- CHECK ASSUMPTIONS --------------------------------------- ##
 ###### ForMod ######
 # checking linear relationship
-par(mfrow=c(3,3))
+par(mfrow=c(2,3))
 termplot(ForMod,se=T,partial.resid = T)
 ## no curvature, so the relationship is linear
 
 # checking if residuals normally distributed
+par(mfrow=c(1,1))
 plot(ForMod,2)
 shapiro.test(resid(ForMod))
 ## from the normal Q-Q plot, the residuals are normally distributed
-## p-value = 0.5073, so the resiudals are normally distributed
+## p-value = 0.5441, so the resiudals are normally distributed
 
 # checking the variance of the residuals 
 ncvTest(ForMod)
-## p-value = 0.63857, so the variance of residuals is constant
+## p-value = 0.64777, so the variance of residuals is constant
 
 # checking the independence of residuals
 durbinWatsonTest(ForMod)
-## p-value = 0.59, rediduals are independent
+## p-value > 0.05, rediduals are independent
 
 # checking collinearity 
 vif(ForMod)
@@ -26,27 +26,27 @@ vif(ForMod)
 
 ###### StepMod ######
 # checking linear relationship
-par(mfrow=c(3,3))
+par(mfrow=c(2,3))
 termplot(StepMod,se=T,partial.resid = T)
 ## no curvature, so the relationship is linear
 
 # checking if residuals normally distributed
+par(mfrow=c(1,1))
 plot(StepMod,2)
 shapiro.test(resid(StepMod))
 ## from the normal Q-Q plot, the residuals are normally distributed
-## p-value = 0.554, so the resiudals are normally distributed
+## p-value = 0.5127, so the resiudals are normally distributed
 
 # checking the variance of the residuals 
 ncvTest(StepMod)
-## p-value = 0.70392, so the variance of residuals is constant
+## p-value = 0.79342, so the variance of residuals is constant
 
 # checking the independence of residuals 
 durbinWatsonTest(StepMod)
-## p-value = 0.67, rediduals are independent
+## p-value > 0.05, rediduals are independent
 
 # checking collinearity 
 vif(StepMod)
 ## 1 < GVIFs < 2, no strong colinearity
 
-
-############### Both ForMod and StepMod pass the assumption checking #################
+# ------------------------------------ BOTH PASS THE CHECKING ------------------------------------ ##
